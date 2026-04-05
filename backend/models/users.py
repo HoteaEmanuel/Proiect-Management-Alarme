38494472d35e8d839ea52425a67342b300688f5a
DariusJderu1 <jderudarius@gmail.com>
2026-04-05 21:08:17 +0300

    Fix user table structure

--- backend/models/users.py
+++ backend/models/users.py
@@ -2,12 +2,11 @@ from database import Base
 from sqlalchemy import Column, Integer, String
 
 class Users(Base):
-
     __tablename__ = "Users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True)
-    username = Column(String, unique=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    hashed_password = Column(String)
+    email = Column(String(100), unique=True)
+    username = Column(String(50), unique=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    hashed_password = Column(String(255)) 
