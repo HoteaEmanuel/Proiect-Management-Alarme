57aa1587b939305d1d4ca6cf2ac871ed3e7844cf
DariusJderu1 <jderudarius@gmail.com>
2026-04-05 20:41:38 +0300

    Add auth.py

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-from routers import alarms
+from routers import alarms, auth
 from database import get_db
-app=FastAPI()
+
+app = FastAPI()
+
 @app.get("/")
 def greet():
     return "Hello"
 
 app.include_router(alarms.router, prefix="/alarms", tags=["Alarms"])
-
+app.include_router(auth.router)
 
 print("HELLO :)")
---
+++ backend/models/users.py
@@ -0,0 +1,13 @@
+from database import Base
+from sqlalchemy import Column, Integer, String
+
+class Users(Base):
+
+    __tablename__ = "Users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
