17de91869de8900c975c41d85f618f42d70f81c8
DariusJderu1 <jderudarius@gmail.com>
2026-04-12 16:53:14 +0300

    Add styles for home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { IoAlertSharp } from "react-icons/io5";
 import Button from '../components/Button';
 import { useNavigate } from 'react-router-dom';
+import '../styles/pages/Home.css'
+
 const Home = () => {
 
   const navigate=useNavigate();
   return (
-    <div className='w-screen h-screen flex flex-col items-center justify-center gap-10'>
-      <h1 className='text-5xl font-bold'>Monitor Smarter. Respond Faster</h1>
-      <h1 className='text-4xl font-bold'>Visualize network issues</h1>
-      <button onClick={()=>navigate('/login')} className='btn w-1/10 h-15 shadow-2xl'>Get started</button>
+    <div className="home-page">
+      <h1 className="home-title">Monitor Smarter. Respond Faster</h1>
+      <h1 className="home-subtitle">Visualize network issues</h1>
+      <button className="home-button" onClick={()=>navigate('/login')}>Get started</button>
     </div>
   )
 }
