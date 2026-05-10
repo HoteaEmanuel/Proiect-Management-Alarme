eaae1a57878f4849a51e8febe00fafc186a345b6
DariusJderu1 <jderudarius@gmail.com>
2026-05-10 20:46:32 +0300

    Adauga logica props GraficBara.jsx

--- frontend/src/features/dashboard/components/statistics/GraficBara.jsx
+++ frontend/src/features/dashboard/components/statistics/GraficBara.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ResponsiveContainer, BarChart, CartesianGrid, XAxis,YAxis,Tooltip,Bar } from "recharts";
 
-const GraficBara = ({ data, direction, vertical }) => {
+const GraficBara = ({ data, direction, vertical, onBarClick }) => {
   return (
     <ResponsiveContainer width="100%" height={280}>
       <BarChart data={data} layout={direction} margin={{ top: 8, right: 16, bottom: 8, left: 0 }}>
@@ -9,7 +9,15 @@ const GraficBara = ({ data, direction, vertical }) => {
         <XAxis dataKey="name" />
         <YAxis />
         <Tooltip />
-        <Bar dataKey="value" radius={[4, 4, 0, 0]} />
+        <Bar 
+          dataKey="value" 
+          radius={[4, 4, 0, 0]} 
+          onClick={(entry) => {
+
+            if(onBarClick)
+              onBarClick(entry.name);
+          }}
+        />
       </BarChart>
     </ResponsiveContainer>
   );
